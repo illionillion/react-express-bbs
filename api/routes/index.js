ed78c5919cdbebd9006c682bd730924fdfe71b92
yuto ichinose <yuto.0526.sano@gmail.com>
2024-06-09 18:03:48 +0900

    fix: 置き換え処理

--- api/routes/index.js
+++ api/routes/index.js
@@ -15,7 +15,14 @@ router.get('/api/post', async (req, res, next) => {
       "select post_id, user_name, user_email, content, posted_at from posts";
     // SQL実行
     const [result] = await connection.execute(query);
-    res.status(200).json({posts: result})
+    const posts = result.map(post => ({
+      postId: post.post_id,
+      userName: post.user_name,
+      userEmail: post.user_email,
+      content: post.content,
+      postedAt: post.posted_at
+    }))
+    res.status(200).json({posts: posts})
   } catch (error) {
       console.error('/api/post Error:', error);
       res.status(500).json({message: "server error"})
