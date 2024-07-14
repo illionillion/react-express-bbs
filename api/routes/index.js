3e6fc8c245be8fd59d284e487fc57ba861333acd
yuto ichinose <yuto.0526.sano@gmail.com>
2024-07-14 20:27:51 +0900

    feat: ログインのAPI実装

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,59 +1,90 @@
-var express = require('express');
-const mysqlConnection = require('../lib/db');
+var express = require("express");
+const mysqlConnection = require("../lib/db");
+const { comparePassword } = require("../lib/password");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
 // get posts
-router.get('/api/post', async (req, res, next) => {
-  let connection = await mysqlConnection()
+router.get("/api/post", async (req, res, next) => {
+  let connection = await mysqlConnection();
   try {
     const query =
       "select p.post_id, u.user_id, u.user_name, p.content, p.posted_at from posts p join users u on p.user_id = u.user_id";
     // SQL実行
     const [result] = await connection.execute(query);
-    const posts = result.map(post => ({
+    const posts = result.map((post) => ({
       postId: post.post_id,
       userId: post.user_id,
       userName: post.user_name,
       content: post.content,
-      postedAt: post.posted_at
-    }))
-    res.status(200).json({posts: posts})
+      postedAt: post.posted_at,
+    }));
+    res.status(200).json({ posts: posts });
   } catch (error) {
-      console.error('/api/post Error:', error);
-      res.status(500).json({message: "server error"})
+    console.error("/api/post Error:", error);
+    res.status(500).json({ message: "server error" });
   } finally {
-    if(connection) connection.destroy()
+    if (connection) connection.destroy();
   }
-})
+});
 
 // create post
-router.post('/api/post', async (req, res, next) => {
-  const {userId, content} = req.body
+router.post("/api/post", async (req, res, next) => {
+  const { userId, content } = req.body;
   if (!userId || !content) {
-    res.status(400).json({message: "データが不足しています"})
-    return
+    res.status(400).json({ message: "データが不足しています" });
+    return;
   }
 
-  let connection = await mysqlConnection()
+  let connection = await mysqlConnection();
 
   try {
-    const query =
-      "insert into posts (user_id, content) values (?, ?, ?)";
+    const query = "insert into posts (user_id, content) values (?, ?, ?)";
     // SQL実行
     await connection.execute(query, [userId, content]);
-    res.status(200).json({message: "success"})
+    res.status(200).json({ message: "success" });
   } catch (error) {
-      console.error('/api/post Error:', error);
-      res.status(500).json({message: "server error"})
+    console.error("/api/post Error:", error);
+    res.status(500).json({ message: "server error" });
   } finally {
-    if(connection) connection.destroy()
+    if (connection) connection.destroy();
   }
-  
-})
+});
+
+router.post("/api/login", async (req, res, next) => {
+  const { userName, password } = req.body;
+  if (!userName || !password) {
+    res.status(400).json({ message: "データが不足しています" });
+    return;
+  }
+
+  let connection = await mysqlConnection();
+
+  try {
+    const query = "select user_id, user_name, user_email, password from users where user_name = ?";
+    // SQL実行
+    const [result] = await connection.execute(query, [userName]);
+    if (result.length > 0) {
+      const user = result[0];
+      const passwordMatch = comparePassword(password, user.password);
+      if (passwordMatch) {
+        res.status(200).json({ message: "success login", userId: user.user_id }); // userIdとトークンを一緒に返す。
+      } else {
+        res.status(401).json({ message: "password is incorrect" });
+      }
+    } else {
+      res.status(404).json({ message: "user not found" });
+    }
+  } catch (error) {
+    console.error("/api/post Error:", error);
+    res.status(500).json({ message: "server error" });
+  } finally {
+    if (connection) connection.destroy();
+  }
+});
 
 module.exports = router;
